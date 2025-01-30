0a7dd12d548796ed0757cfe659f4209bf6c4846f
muhammad 123 <aliareq4@gmail.com>
2025-01-31 00:00:30 +0300

    main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const number = 10;
+
+if (number > 0) {
+    console.log("Число положительное");
+} else if (number < 0) {
+    console.log("Число отрицательное");
+} else {
+    console.log("Число ни положительное, ни отрицательное");
+}
