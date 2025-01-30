852a68c302710d95a03649550d8f9f196a151865
muhammad 123 <aliareq4@gmail.com>
2025-01-31 00:42:29 +0300

    .\main.js

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-let num1   = 443
-let num2 =12
-    console.log(`Равны: ${num1 === num2}`);
-    console.log(`Количество цифр: ${String(num1).length + String(num2).length}`);
-    console.log(`Большее: ${Math.max(num1, num2)}`);
-    console.log(`Меньшее: ${Math.min(num1, num2)}`);
-    if(num1 % num2 ===0) {
-        console.log(`${num1} делится на ${num2}`);
-    } else if(num2 % num1 ===0) {
-        console.log(`${num2} делится на ${num1}`)
-    } else {
-        console.log('не делится');
-    }
-
+let num = 999
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (num % 3 === 0) {
+    console.log("Fizz");
+} else if (num % 5 === 0) {
+    console.log("Buzz");
+}
 
