bb0cae0a26572c1ab3145d178386ffdfd3133777
muhammad 123 <aliareq4@gmail.com>
2025-01-31 00:28:26 +0300

    .\main.js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-let n = 123
-    const s = String(n);
-    const sum = parseInt(s[0]) + parseInt(s[s.length - 1]);
-    console.log(`Сумма: ${sum}`);
-    console.log(`Целое: ${Number.isInteger(n)}`);
-    console.log(`Четное: ${n % 2 === 0}`);
+let num1   = 443
+let num2 =12
+    console.log(`Равны: ${num1 === num2}`);
+    console.log(`Количество цифр: ${String(num1).length + String(num2).length}`);
+    console.log(`Большее: ${Math.max(num1, num2)}`);
+    console.log(`Меньшее: ${Math.min(num1, num2)}`);
+    if(num1 % num2 ===0) {
+        console.log(`${num1} делится на ${num2}`);
+    } else if(num2 % num1 ===0) {
+        console.log(`${num2} делится на ${num1}`)
+    } else {
+        console.log('не делится');
+    }
 
 
