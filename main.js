0b0d047f94b58d836d6ce667d01e0147b70b0c98
muhammad 123 <aliareq4@gmail.com>
2025-02-07 16:55:40 +0300

    .\main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const a = ["дима", "петя", "настя","олег","оля"];
-const b = Math.floor(a.length / 2);
-for (let i = b; i <a.length; i++) {
+const numbers = [1, -2, -3, 4, -5, 6, -7, 8, -9, 0];
+let pos = 0;
+let neg = 0;
 
+for (let i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i] > 0) {pos++;}
+    else if (numbers[i] < 0) {neg++;}}
 
-
-console.log(a[i]);
-}
+console.log("Количество положительных чисел:", pos)
+console.log("Количество отрицательных чисел:", neg)
