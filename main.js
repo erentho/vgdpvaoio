213cc47d6fd1f4d98bb4de27da3d56fe3550f015
muhammad 123 <aliareq4@gmail.com>
2025-02-14 00:57:45 +0300

    сделал через i++

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const numbers = [1, -2, -3, 4, -5, 6, -7, 8, -9, 0];
-let pos = 0;
-let neg = 0;
 
-for (let i = 0; i < numbers.length - 1; i++) {
-    if (numbers[i] > 0) {pos++;}
-    else if (numbers[i] < 0) {neg++;}}
 
-console.log("Количество положительных чисел:", pos)
-console.log("Количество отрицательных чисел:", neg)
+let height = 5;
+for (let i = 1; i <= height; i++) {
+    const spacecount = height - i;
+    const linenumber = i;
+    const starcount = (linenumber * 2) - 1;
+
+    console.log(" ".repeat(spacecount) + "*".repeat(starcount));
+}
+
+
+
+
