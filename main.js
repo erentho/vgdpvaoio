92dcb662934d3ece2bfe08b957d2f71b241a0224
muhammad 123 <aliareq4@gmail.com>
2025-02-14 01:15:17 +0300

    матрица

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+const matrix = [
+    ['Id', 'Title', 'Description'],
+    [1, 'Title-1', 'Description-1'],
+    [2, 'Title-2', 'Description-2'],
+    [3, 'Title-3', 'Description-3', 'Поле с багом'],
+    [4, 'Title-4', 'Description-4'],
+    [5, 'Title-5', 'Description-5'],
+    [6, 'Title-6', 'Description-6', 'Поле с багом'],
+];
+for (let i = 0; i < matrix.length; i++) {
+  const arr = matrix[i];
+  let text = ``
+    for (let j = 0; j < arr.length; j++) {
+        const element = arr[j];
+        text += element + ` `;
+
+    }
+
+    console.log(text);
 
 
-let height = 5;
-for (let i = 1; i <= height; i++) {
-    const spacecount = height - i;
-    const linenumber = i;
-    const starcount = (linenumber * 2) - 1;
-
-    console.log(" ".repeat(spacecount) + "*".repeat(starcount));
 }
-
-
-
-
