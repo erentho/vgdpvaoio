a5633d861777f2d6beef21752283b961d4cf4ef1
muhammad 123 <aliareq4@gmail.com>
2025-02-19 16:06:01 +0300

    for of

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-
-const person = { age: 27, name: 'Billy', child: 'Max' };
-const keys = [];
-const values = []
-for (const key in person) {
-
-        {keys.push(key);
-        values.push(person[key]);}
+const numbers = [10, -5, 20, 0, 15];
 
 
+    let min = numbers[0];
+    let max = numbers[0];
+    let sum = 0;
 
+    for (const num of numbers) {
+        min = Math.min(min, num);
+        max = Math.max(max, num);
+        sum += num;
+
+
+    const avg = sum / numbers.length;
+
+    console.log("Мин:", min);
+    console.log("Макс:", max);
+    console.log("Среднее:", avg);
 }
-
-console.log("Ключи:", keys);
-console.log("Значения:", values);
