bda47c49eb88131ad1ebc3a4cc618d5c1d9a953d
muhammad 123 <aliareq4@gmail.com>
2025-02-19 16:36:54 +0300

    for of алфавит

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-const numbers = [10, -5, 20, 0, 15];
 
+const гласные = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'];
+const всегоБукв = 33;
+const количествоГласных = гласные.length;
+const количествоСогласных = всегоБукв - количествоГласных;
 
-    let min = numbers[0];
-    let max = numbers[0];
-    let sum = 0;
-
-    for (const num of numbers) {
-        min = Math.min(min, num);
-        max = Math.max(max, num);
-        sum += num;
-
-
-    const avg = sum / numbers.length;
-
-    console.log("Мин:", min);
-    console.log("Макс:", max);
-    console.log("Среднее:", avg);
-}
+console.log("Гласные в алфавите:", количествоГласных);
+console.log("Согласные в алфавите:", количествоСогласных);
