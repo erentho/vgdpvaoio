0ee803fb0bcd1a442a8b829765fc584b3da5bb2b
muhammad 123 <aliareq4@gmail.com>
2025-02-19 18:06:05 +0300

    for if максимальная длина

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
+const user = {
+    example: [1, 2, 3],
+    'X-Address': {
+        street: 'Ленина',
+        house: '10-Б',
+    },
+    nickname: null,
+    permissions: [true, false],
+    age: 101,
+    emails: ['alice@example.com', 'bob@example.com', 'charlie@example.com', 'peter@example.com'],
+    name: 'Peter Charles',
+    isAdult: true,
+    now: new Date().toISOString(),
+};
 
-const гласные = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'];
-const всегоБукв = 33;
-const количествоГласных = гласные.length;
-const количествоСогласных = всегоБукв - количествоГласных;
+let maxLength = 0;
+let longestArray = null;
+let lastElement = null;
 
-console.log("Гласные в алфавите:", количествоГласных);
-console.log("Согласные в алфавите:", количествоСогласных);
+        for (const key in user) {
+        if (Array.isArray(user[key])) {
+
+        const arrayLength = user[key].length;
+        if (arrayLength > maxLength) {
+
+            maxLength = arrayLength;
+
+            longestArray = user[key];
+
+            lastElement = user[key][arrayLength - 1];
+        }
+    }
+}
+
+console.log(`Максимальная длина: ${maxLength}. Последнее значение: ${lastElement}`);
