1b10a19673e6ce8c43a97b4cf5a2d3c3bbb91d2f
muhammad 123 <aliareq4@gmail.com>
2025-02-25 23:37:01 +0300

    достучались

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,61 @@
 type A = {
-  age?: number | string;
-  nick: string;
-  info: { [key: string]: any } | any[];
-  photo?: string | null;
+  first: string;
+  second: {
+    third: string;
+    fourth?: string;
+    fifth: {
+      sixth?: {
+        tenth: {
+          twelfth?: {
+            thirteenth: {
+              fifteenth: {
+                sixteenth?: {
+                  last: 'Достучались';
+                };
+              };
+            };
+            fourteenth: string;
+          };
+        };
+        eleventh: string;
+      };
+      seventh: number;
+      eight: string;
+    };
+  };
 };
 
-const a1: A = {
-  age: 10,
-  nick: 'mynick',
-  info: [1, 100],
-  photo: 'string',
+const a: A = {
+  first: 'z',
+  second: {
+    third: 'f',
+    fourth: 'a',
+    fifth: {
+      sixth: {
+        tenth: {
+          twelfth: {
+            thirteenth: {
+              fifteenth: {
+                sixteenth: {
+                  last: 'Достучались',
+                },
+              },
+            },
+            fourteenth: 's',
+          },
+        },
+        eleventh: 'kk',
+      },
+      seventh: 8,
+      eight: 'b',
+    },
+  },
 };
 
-const a2: A = {
-  age: '10 years',
-  nick: '',
-  info: { id: 100 },
-  photo: null,
-};
+const ласт = a.second.fifth.sixth?.tenth.twelfth?.thirteenth.fifteenth.sixteenth?.last;
 
-const a3: A = {
-  nick: '       ',
-  info: ['secret', 'key'],
-};
-
-const a4: A = {
-  nick: '',
-  info: {},
-};
-
-console.log(a1.age);
-console.log(a2.age);
-console.log(a3.info);
-console.log(a4.info);
+if (ласт) {
+  console.log(ласт.toUpperCase());
+} else {
+  console.log('undefined');
+}
