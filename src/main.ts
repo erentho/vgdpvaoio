8ee7173721559a5ea8e866492a2295f04dcfdff3
muhammad 123 <aliareq4@gmail.com>
2025-02-25 23:31:06 +0300

    типы 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
-console.log('Hello World!');
+type A = {
+  age?: number | string;
+  nick: string;
+  info: { [key: string]: any } | any[];
+  photo?: string | null;
+};
+
+const a1: A = {
+  age: 10,
+  nick: 'mynick',
+  info: [1, 100],
+  photo: 'string',
+};
+
+const a2: A = {
+  age: '10 years',
+  nick: '',
+  info: { id: 100 },
+  photo: null,
+};
+
+const a3: A = {
+  nick: '       ',
+  info: ['secret', 'key'],
+};
+
+const a4: A = {
+  nick: '',
+  info: {},
+};
+
+console.log(a1.age);
+console.log(a2.age);
+console.log(a3.info);
+console.log(a4.info);
