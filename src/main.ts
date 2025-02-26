ce084b65a31176ff647b09099b78c9bf017c6ff9
muhammad 123 <aliareq4@gmail.com>
2025-02-26 16:31:13 +0300

    ГИРЛЯНДА

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,14 @@
-type A = {
-  first: string;
-  second: {
-    third: string;
-    fourth?: string;
-    fifth: {
-      sixth?: {
-        tenth: {
-          twelfth?: {
-            thirteenth: {
-              fifteenth: {
-                sixteenth?: {
-                  last: 'Достучались';
-                };
-              };
-            };
-            fourteenth: string;
-          };
-        };
-        eleventh: string;
-      };
-      seventh: number;
-      eight: string;
-    };
-  };
-};
+import { blue, green, red } from 'chalk';
 
-const a: A = {
-  first: 'z',
-  second: {
-    third: 'f',
-    fourth: 'a',
-    fifth: {
-      sixth: {
-        tenth: {
-          twelfth: {
-            thirteenth: {
-              fifteenth: {
-                sixteenth: {
-                  last: 'Достучались',
-                },
-              },
-            },
-            fourteenth: 's',
-          },
-        },
-        eleventh: 'kk',
-      },
-      seventh: 8,
-      eight: 'b',
-    },
-  },
-};
+const colors = [red, green, blue];
+const max = 10;
 
-const ласт = a.second.fifth.sixth?.tenth.twelfth?.thirteenth.fifteenth.sixteenth?.last;
-
-if (ласт) {
-  console.log(ласт.toUpperCase());
-} else {
-  console.log('undefined');
+let result = '';
+for (let i = 0; i < max; i++) {
+  result += colors[i % 3]('*');
+  if (i < max - 1) {
+    result += ' - ';
+  }
 }
+
+console.log(result);
