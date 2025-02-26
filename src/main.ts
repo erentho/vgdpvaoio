51e193129e24713060deffc3197cd6c9978ab261
muhammad 123 <aliareq4@gmail.com>
2025-02-26 17:27:36 +0300

    литералы

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
-type operatingSystem = 'windows' | 'macOs' | 'linux'; // строковый
-type pcNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9; // числовой
+/*
+1ая группа: 12,2,9,5
+2ая группа: 4,6,10,8
+3я группа: 3,7,11,1
+*/
 
-type pc = {
-  os: operatingSystem;
-  num: pcNumber;
-  createdAt: Date;
-  createdAtISO: string;
-}; // объект
+import dayjs from 'dayjs';
 
-const myPc: pc = {
-  os: 'windows',
-  num: 4,
-  createdAt: new Date(),
-  createdAtISO: new Date().toISOString(),
-};
+console.log(12, dayjs('2024-08-23T12:30:00Z').toISOString());
+console.log(2, dayjs('2024-08-23T15:30:00+03:00').toISOString());
+console.log(5, dayjs('2024-08-24T01:30:00+13:00').toISOString());
+console.log(9, dayjs('2024-08-23T09:30:00-03:00').toISOString());
+console.log('----\n');
 
-console.log(myPc); // дата ....
+console.log(4, dayjs('2024-08-23T15:30:00Z').toISOString());
+console.log(6, dayjs('2024-08-23T17:30:00+02:00').toISOString());
+console.log(10, dayjs('2024-08-23T14:30:00-01:00').toISOString());
+console.log(8, dayjs('2024-08-23T03:30:00-12:00').toISOString());
+console.log('----\n');
+
+console.log(1, dayjs('2024-08-23T03:30:00-11:00').toISOString());
+console.log(3, dayjs('2024-08-23T14:30:00Z').toISOString());
+console.log(7, dayjs('2024-08-23T10:30:00-04:00').toISOString());
+console.log(11, dayjs('2024-08-24T02:30:00+12:00').toISOString());
