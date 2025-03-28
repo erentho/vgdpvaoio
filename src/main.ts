bbc6c13a50f2807e7d0c5b55673d36bf052aa20a
muhammad 123 <aliareq4@gmail.com>
2025-03-28 14:50:27 +0300

    reduce

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
-const mergeToNewObject = (a: any, b: any) => {
-  return { ...a, ...b };
-};
+// Используя reduce, напишите функцию, которая (каждый пункт отдельная функция):
+// Фильтрует только положительные числа
+// Создаёт массив только с уникальными значениями
+// Создает обратный массив (у которого обратная последовательность элемента)
+// Превращает массив в объект:
 
-const obj1 = { a: 10, b: 100, c: 1 };
-const obj2 = { a: 20, b: 200, e: 2 };
-const obj3 = mergeToNewObject(obj1, obj2);
+// Фильтрует только положительные числа
+const filterPositiveNumbers = (arr: number[]): number[] =>
+  arr.reduce((acc: number[], num: number) => (num > 0 ? [...acc, num] : acc), []);
 
-console.log(obj1); // { a: 10, b: 100, c: 1 }
-console.log(obj2); // { a: 20, b: 200, e: 2 }
-console.log(obj3); // { a: 20, b: 200, c: 1, e: 2 }
+// Фильтрует только строки
+const filterStrings = (arr: any[]): string[] =>
+  arr.reduce((acc: string[], item: any) => (typeof item === 'string' ? [...acc, item] : acc), []);
+
+// Создаёт массив только с уникальными значениями
+const getUniqueValues = (arr: any[]): any[] =>
+  arr.reduce((acc: any[], item: any) => (acc.includes(item) ? acc : [...acc, item]), []);
+
+// Создает обратный массив (у которого обратная последовательность элемента)
+const reverseArray = (arr: any[]): any[] => arr.reduce((acc: any[], item: any) => [item, ...acc], []);
+
+// Превращает массив в объект:
+const values = [10, 'just_name', true];
+const myObject = values.reduce(
+  (acc: { [key: string]: any }, value: any, index: number) => ({ ...acc, [`key-${index + 1}`]: value }),
+  {},
+);
+console.log(myObject);
+// { 'key-1': 10, 'key-2': 'just_name', 'key-3': true }
