78664b3c1ab965e6a0e0eff8e63bd452ead2ee71
muhammad 123 <aliareq4@gmail.com>
2025-03-28 17:31:56 +0300

    деструктуризация м

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
-const values = [
-  [1, 100],
-  [2, 200],
-  [3, 300],
-  [1, 2],
-  [3, 400],
-  [4, 500],
-  [1, 99],
+const storage = [
+  'nick',
+  'nack',
+  'nock',
+  [
+    {
+      first: 'forecast',
+      child: null,
+    },
+    {
+      first: 'castfore',
+      child: null,
+    },
+    'zzz',
+  ],
+  'no-1',
+  'no-2',
 ];
 
-const result = values.reduce((acc: { [key: string]: { count: number; sum: number } }, [grade, count]) => {
-  const gradeStr = String(grade);
-  acc[gradeStr] = {
-    count: (acc[gradeStr]?.count || 0) + 1,
-    sum: (acc[gradeStr]?.sum || 0) + count,
-  };
-  return acc;
-}, {});
+const [, , , [a, b, ...c], ...d] = storage;
+const { first: f1 } = a as { first: string; child: null };
 
-console.log(result);
+console.log(d);
+console.log(c);
+console.log(b);
